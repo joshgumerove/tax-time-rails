c7b60ed88f3b8ee8d03c7fb7f594917e6b05cb57
joshgumerove <jgumerove@kaufmanrossin.com>
2022-04-28 11:18:43 -0400

    custom route

--- app/controllers/api/returns_controller.rb
+++ app/controllers/api/returns_controller.rb
@@ -2,12 +2,17 @@ class Api::ReturnsController < ApplicationController
 
     def index
         returns = Client.find_by('id').returns
-        render json: {status: 'SUCCESS', message: 'LOADED RETURNS', data: returns}
+        render json: {status: 'SUCCESS', message: 'LOADED CLIENT RETURNS', data: returns}
     end
 
     def show
         returns = Client.find_by('id').returns.find_by('id') #cannot use return as a variable
         render json: {status: 'SUCCESS', message: 'LOADED CLIENT', data: returns}
+    end
+
+    def client_returns
+        returns = Return.all 
+        render json: {status: "SUCCESS", message: "LOADED RETURNS", data: returns}
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace 'api' do
+    get '/returns', to: 'returns#client_returns'
     resources :clients do
-      resources :returns
+      resources :returns do 
+      end
     end
   end
 
