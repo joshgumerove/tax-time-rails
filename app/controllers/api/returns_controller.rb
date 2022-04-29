e0b59b1eca84d951fadb2600ce9f85c85c12789a
joshgumerove <jgumerove@kaufmanrossin.com>
2022-04-29 12:48:08 -0400

    fix routing

--- app/controllers/api/returns_controller.rb
+++ app/controllers/api/returns_controller.rb
@@ -1,18 +1,32 @@
 class Api::ReturnsController < ApplicationController
 
     def index
-        returns = Client.find_by('id').returns
-        render json: {status: 'SUCCESS', message: 'LOADED CLIENT RETURNS', data: returns}
+        @returns = Return.all
+        @returns = @returns.where(client_id: params[:client_id]) if params[:client_id]
+
+        render json: {status: 'SUCCESS', message: 'LOADED CLIENT RETURNS', data: @returns}
     end
 
     def show
-        returns = Client.find_by('id').returns.find_by('id') #cannot use return as a variable
+        returns = Return.find_by(id: params[:id]) #cannot use return as a variable
         render json: {status: 'SUCCESS', message: 'LOADED CLIENT', data: returns}
+    end
+
+    def create
+        tax_return = Return.create(return_params)
+        render json: {data: tax_return}
     end
 
     def client_returns
         returns = Return.all 
         render json: returns, except: [:created_at, :updated_at]
     end
+
+    private 
+
+    def return_params
+       params.require(:return).permit(:return_type, :client_id, :description, :time_estimate, :due_date)
+    end
+
 
 end
